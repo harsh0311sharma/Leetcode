cb662858783a571749f034f50e596d8971fd452e
harsh0311sharma <harshmurli1234@gmail.com>
2026-07-18 22:22:43 +0530

    palindrome

---
+++ basicmaths/palindrome/palindrome.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class palindrome{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        int n  = sc.nextInt();
+
+        int dup=n;
+        int reverseno = 0;
+        while(n>0){
+            int lastdigit = n % 10;
+           
+            n  = n /10;
+            reverseno = (reverseno*10)+lastdigit;
+        }
+       if(reverseno==dup){
+        System.out.println("true");
+       } else{
+        System.out.println("false");
+       }
+    }
+} 
